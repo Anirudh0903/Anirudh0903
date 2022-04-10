ecf6c16f2632b2ddc17290b98b61f8c23ad65602
Anirudh0903 <103094953+Anirudh0903@users.noreply.github.com>
2022-04-10 17:38:09 +0530

    My First Trial

---
+++ P1_Task_01_HelloJava.java
@@ -0,0 +1,10 @@
+package Unit_01;
+
+public class P1_Task_01_HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello\n I am Anirudh bhatt, Sec : H , Roll no : 13 \n This is my 1st program");
+
+	}
+
+}
